5604c99f692621c81516c1a4a88fe1e69ca618e4
wkdgus1164 <wkdgus1164@naver.com>
2022-09-06 19:55:38 +0900

    refactor: Dialog 컴포넌트 분리

--- src/common/components/Appbar/responsive-menu/TabletMenubar.tsx
+++ src/common/components/Appbar/responsive-menu/TabletMenubar.tsx
@@ -1,101 +1,14 @@
 import { useState } from 'react'
-import {
-  AppBar,
-  Button,
-  Dialog,
-  IconButton,
-  Stack,
-  Toolbar,
-  Typography,
-} from '@mui/material'
+import { IconButton, Stack } from '@mui/material'
 import { Profile } from '@/common/components/Appbar/Profile'
-import { ClearRounded, MenuRounded, SearchRounded } from '@mui/icons-material'
+import { MenuRounded, SearchRounded } from '@mui/icons-material'
 import { css } from '@emotion/react'
-import Link from 'next/link'
+import { MenuDialog } from '@/common/components/Appbar/responsive-menu/MenuDialog'
 
 export const TabletMenubar = () => {
   const [open, setOpen] = useState(false)
   const handleClickOpen = () => setOpen(true)
   const handleClose = () => setOpen(false)
-
-  const MenuDialog = () => (
-    <Dialog open={open} fullScreen onClose={handleClose}>
-      <AppBar position='relative'>
-        <Toolbar css={style.toolbar}>
-          <Stack css={style.wrapper}>
-            <Link href='/'>
-              <img
-                src='/logo.svg'
-                alt=''
-                style={{
-                  width: '126.3px',
-                  height: '23.6px',
-                  cursor: 'pointer',
-                }}
-              />
-            </Link>
-
-            <Stack direction='row'>
-              <Profile isLogin />
-              <IconButton css={style.icon}>
-                <SearchRounded />
-              </IconButton>
-              <IconButton
-                css={style.icon}
-                sx={{
-                  marginLeft: 1.25,
-                  backgroundColor: 'black',
-                  ':hover': {
-                    backgroundColor: 'black',
-                  },
-                }}
-                onClick={handleClose}
-              >
-                <ClearRounded sx={{ color: 'white' }} />
-              </IconButton>
-            </Stack>
-          </Stack>
-        </Toolbar>
-      </AppBar>
-
-      <Stack mt={5} mr={3}>
-        <Link href='/'>
-          <Typography variant='h3' fontWeight='bold' textAlign='right'>
-            Curated Collections
-          </Typography>
-        </Link>
-        <Link href='/'>
-          <Typography variant='h3' fontWeight='bold' textAlign='right'>
-            NFTs
-          </Typography>
-        </Link>
-        <Link href='/'>
-          <Typography variant='h3' fontWeight='bold' textAlign='right'>
-            Editorial
-          </Typography>
-        </Link>
-        <Link href='/'>
-          <Typography variant='h3' fontWeight='bold' textAlign='right'>
-            Sign Out
-          </Typography>
-        </Link>
-      </Stack>
-
-      <Stack direction='row' flexDirection='row-reverse' mr={3} mt={3.5}>
-        <IconButton>
-          <img src='/twitter-black.svg' alt='' />
-        </IconButton>
-        <IconButton>
-          <img src='/instagram-black.svg' alt='' />
-        </IconButton>
-        <IconButton>
-          <img src='/youtube-black.svg' alt='' />
-        </IconButton>
-      </Stack>
-
-      <Button css={style.dialogButton}>English</Button>
-    </Dialog>
-  )
 
   return (
     <Stack direction='row'>
@@ -110,7 +23,7 @@ export const TabletMenubar = () => {
       >
         <MenuRounded />
       </IconButton>
-      <MenuDialog />
+      <MenuDialog open={open} onClose={handleClose} />
     </Stack>
   )
 }
