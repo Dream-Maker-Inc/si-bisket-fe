717149137c6724960657d66b81ea5837019af121
andy <wkdgus1164@naver.com>
2022-09-07 19:09:58 +0900

    refactor: 빌드 세팅

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import("next").NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true
-};
+  swcMinify: true,
+  images: {
+    loader: 'akamai',
+    path: './',
+  },
+  assetPrefix: '.',
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
