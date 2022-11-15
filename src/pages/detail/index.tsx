60e073755ee8be93763d259de6c7d31c7a156404
schatzen <gomieti@naver.com>
2022-11-15 14:58:00 +0900

    feat: form section 구현

--- src/pages/detail/index.tsx
+++ src/pages/detail/index.tsx
@@ -2,12 +2,14 @@ import type { NextPage } from 'next'
 import { Footer } from '@/common/components/footer'
 import { BannerSection } from '@/domain/detail/sections/banner'
 import { DescriptionSection } from '@/domain/detail/sections/description'
+import { FormSection } from '@/domain/detail/sections/form'
 
 const Detail: NextPage = () => (
   <div>
     <main>
       <BannerSection />
       <DescriptionSection />
+      <FormSection />
     </main>
     <Footer />
   </div>
