6bba5923da5ec338c2df238d3391cf15bc57fe60
schatzen <gomieti@naver.com>
2022-11-17 12:17:03 +0900

    feat: 배너 이미지 추가

--- src/domain/detail/sections/banner/BannerSection.tsx
+++ src/domain/detail/sections/banner/BannerSection.tsx
@@ -1,10 +1,15 @@
 import { Colors } from '@/common/themes/Color'
+import { MediaQueries } from '@/common/themes/Limit'
 import { css } from '@emotion/react'
 
 export const BannerSection = () => {
   return (
     <div css={style.root}>
-      <img src='/detail/banner/main-banner.png' css={style.image} />
+      <div css={style.imageContainer}>
+        <img src='/detail/banner/main-banner.png' css={style.image} />
+        <img src='/detail/banner/rec-banner.png' css={style.rec} />
+        <img src='/detail/banner/vertical-banner.png' css={style.vertical} />
+      </div>
     </div>
   )
 }
@@ -12,14 +17,66 @@ export const BannerSection = () => {
 const style = {
   root: css`
     width: 100%;
-    aspect-ratio: 1/0.411;
+    height: 870px;
     display: flex;
     justify-content: center;
     align-items: center;
     background-color: ${Colors.BannerBg};
+
+    @media ${MediaQueries.md} {
+      height: 560px;
+    }
+
+    @media ${MediaQueries.xs} {
+      height: 400px;
+    }
+  `,
+  imageContainer: css`
+    width: 792px;
+    height: 576px;
+    position: relative;
+
+    @media ${MediaQueries.xl} {
+      width: 92%;
+      height: unset;
+      aspect-ratio: 1/0.727;
+    }
   `,
   image: css`
-    max-width: 630px;
+    width: 576px;
     aspect-ratio: 1/1;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 3;
+
+    @media ${MediaQueries.xl} {
+      width: 72.7%;
+    }
+  `,
+  rec: css`
+    width: 792px;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    position: absolute;
+    z-index: 2;
+
+    @media ${MediaQueries.xl} {
+      width: 100%;
+    }
+  `,
+  vertical: css`
+    width: 792px;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    position: absolute;
+    z-index: 1;
+
+    @media ${MediaQueries.xl} {
+      width: 100%;
+    }
   `,
 }
